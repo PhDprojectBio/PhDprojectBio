7d7fe64dd9a87aba3345c583464fd1c3ec477fed
PhDprojectBio <137326512+PhDprojectBio@users.noreply.github.com>
2024-05-31 18:51:48 +0200

    Update 4b. Finishing - summary values and exporting shapes to GIS.R

--- Scripts Norwegian and Greenland occurrences/4b. Finishing - summary values and exporting shapes to GIS.R
+++ Scripts Norwegian and Greenland occurrences/4b. Finishing - summary values and exporting shapes to GIS.R
@@ -47,45 +47,15 @@ for (i in 1:length(namesShallowDeep)){
 
 
 
-
-
-
-
-
-
-
-sp_list_class_dom <- sp_list2 %>% arrange(class) %>% group_by(class) %>% mutate(maximum = max(abundance)) %>% filter(abundance == maximum)
-dom_sp_class <- sp_list_class_dom$scientificName
-sp_list_class_dom2 <- dbs_mkd_taxa_new %>% arrange(class) %>% group_by(class) %>% subset(scientificName %in% dom_sp_class)
-sp_list_class_shp <- dbs_mkd_taxa_new %>% arrange(class) %>% group_by(class) %>% group_split()
-
-
-length(sp_list_class_shp)
-
-for (i in 1:length(sp_list_class_shp)){
-  class_shp <- sp_list_class_shp[[i]]
-  name <- class_shp$class[1]
-  write.table(class_shp, file = gsub(" ","", paste("matlab_",name,".txt")), sep = "\t", na = "NA", dec = ".", row.names = FALSE, quote = FALSE)
-  #assign(gsub(" ","", paste("shp_",name)), class_shp)
+#V. Converting to shapes, an example
   
-  shp <- st_as_sf(x = class_shp,                         
+  shp <- st_as_sf(x = dataset,                         
                   coords = c("XCoord", "YCoord"),
                   crs = 4326)
   
   st_write(shp, gsub(" ","", paste("shp_",name,".shp")))
 }
-
 rm(i)
 
-shp <- st_as_sf(x = sp_list_class_dom2,                         
-                coords = c("XCoord", "YCoord"),
-                crs = 4326)
-
-st_write(shp, "dominant_taxa_class.shp")
-
-
-  
-write.table(sp_list, file = "splist.txt", sep = "\t", na = "NA", dec = ".", row.names = FALSE, quote = FALSE)
-write.table(sp_list_class_dom, file = "splist_dom.txt", sep = "\t", na = "NA", dec = ".", row.names = FALSE, quote = FALSE)
 
 
